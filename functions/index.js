ffa5242c64530cfccb130c9d3a38d7626dce2d44
Jhosehp Rendon <jhosehp.rendon@gmail.com>
2018-12-10 09:36:49 -0500

    Invalidating used codes

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,8 @@ const admin = require('firebase-admin')
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
 const serviceAccount = require('./service_account.json');
-const requestOneTimePassword = require('./request-one-time-password');
+const requestOneTimePassword = require('./request_one_time_password');
+const verifyOneTimePassword = require('./verify_one_time_password');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -11,4 +12,6 @@ admin.initializeApp({
 
 exports.createUser = functions.https.onRequest(createUser);
 exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword);
+
 
